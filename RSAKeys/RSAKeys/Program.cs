7c5bc00af76f4e343956413345b0b2f0d928e6e4
Margaret <ri.tysik@mail.ru>
2020-01-16 01:34:03 +0300

    Add RSA algorithm

---
+++ RSAKeys/RSAKeys/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 29;
+            int q = 73;
+            var text = "CBC";
+
+            var rsa = new RSA(p, q);
+            Console.WriteLine($"string: {text}");
+
+            var encrypted = rsa.Encrypt(text);
+            Console.WriteLine($"encrypted: {encrypted}");
+
+            var decrypted = rsa.Decrypt(encrypted);
+            Console.WriteLine($"decrypted: {decrypted}");
+        }
+    }
+}
---
+++ RSAKeys/RSAKeys/RSA.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAKeys
+{
+    public class RSA
+    {
+        private readonly int p;
+        private readonly int q;
+        private int n;
+        private int e;
+        private int d;
+
+        public RSA(int p, int q)
+        {
+            this.p = p;
+            this.q = q;
+            GetPrivateCipherKey();
+        }
+
+        private void GetPrivateCipherKey()
+        {
+            n = p * q;
+            var func = (p - 1) * (q - 1);
+            e = func - 1; // два соседних числа - взаимно простые
+
+            int k = 1;
+            while(true)
+            {
+                if (((k * func) + 1) % e == 0)
+                {
+                    d = ((k * func) + 1)/e;
+                    break;
+                }
+                k++;
+            }
+        }
+
+        public string Encrypt(string text)
+        {
+            List<int> resultCodes = new List<int>();
+
+            for (int i=0; i < text.Length; i++)
+            {
+                resultCodes.Add((text[i]^e) % n);  
+            }
+
+            string result = "";
+            foreach (var code in resultCodes)
+            {
+                result += (char)code;
+            }
+            return result;
+        }
+
+        public string Decrypt(string text)
+        {
+            List<int> resultCodes = new List<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                resultCodes.Add((text[i] ^ d) % n);
+            }
+
+            string result = "";
+            foreach (var code in resultCodes)
+            {
+                result += (char)code;
+            }
+            return result;
+        }
+
+    }
+}
